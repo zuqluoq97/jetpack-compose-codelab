3624297d1e3284881644e6568ec592e8b5e4464f
Luong Tri Dung <zuqluoq97@gmail.com>
2021-05-30 14:19:56 +0700

    Section 4 - Declerative UI

--- app/src/main/java/com/example/ltdung/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/ltdung/jetpackcompose/MainActivity.kt
@@ -3,36 +3,57 @@ package com.example.ltdung.jetpackcompose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ltdung.jetpackcompose.ui.JetpackComposeTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetpackComposeTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    Column {
+        names.forEach {
+            Greeting(name = it)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name")
+    Text(text = "Hello $name", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
